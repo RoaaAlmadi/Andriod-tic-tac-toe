a94f76c030a25f64e2a4f7ee9aadde1295d240f3
burningman2k20 <burningman2k9@gmail.com>
2020-07-09 16:37:55 -0400

    blah

--- app/src/main/java/com/example/ttt/MainActivity.java
+++ app/src/main/java/com/example/ttt/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.ttt;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+//import android.MaterialAlertDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                double x = Math.random();
+                //something(x);
+                System.out.println(getRandomNumberInts(1,9));
+
+
+            }
+        });
     }
+    public static int getRandomNumberInts(int min, int max){
+        Random random = new Random();
+        return random.ints(min,(max+1)).findFirst().getAsInt();
+    }
+
+
 }
